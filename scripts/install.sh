d43fc3652c085294ef77de002b937263f9fc1a0d
Morvaniuk Vitalii <morvanyuk.vitaly@gmail.com>
2016-06-03 00:43:08 +0300

    Update install script #2

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ drush dl drupal-7 --drupal-project-rename=drupal
 mv drupal/* .
 mv drupal/.htaccess .
 rm -rf drupal
-ln -s $ZENCI_DEPLOY_DIR sites/all/ZENCI_REPO_NAME
+ln -s $ZENCI_DEPLOY_DIR sites/all/travis_ci_drupal_module_example
 drush --verbose site-install --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@127.0.0.1/$DATABASE_NAME --yes
 drush --yes pm-enable simpletest
 drush --yes pm-enable travis_ci_drupal_module_example
