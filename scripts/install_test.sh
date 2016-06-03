aa4220d515ef41100b021886c47a9ef311079274
Morvaniuk Vitalii <morvanyuk.vitaly@gmail.com>
2016-06-03 09:35:43 +0300

    Setup tests #9

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -4,12 +4,10 @@ echo "Installing drupal to " . $DOCROOT
 
 DATABASE_PASS="test"
 cd $DOCROOT
-
-drush dl drupal-7 --drupal-project-rename=drupal
-mv drupal/* .
-mv drupal/.htaccess .
 ls -lah
-pwd
+drush dl drupal-7 --drupal-project-rename=drupal
+mv drupal/* $DOCROOT
+mv drupal/.htaccess $DOCROOT
 rm -rf drupal
 ln -s $ZENCI_DEPLOY_DIR sites/all/modules/travis_ci_drupal_module_example
 drush --verbose site-install --db-url=mysql://test:@127.0.0.1/test --yes
