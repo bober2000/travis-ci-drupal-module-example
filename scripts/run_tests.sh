bfed5367b35c22e40c3bd701b570129f4383365e
Morvaniuk Vitalii <morvanyuk.vitaly@gmail.com>
2016-06-03 09:59:03 +0300

    Setup tests #14

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,4 +4,4 @@ SITEPATH="$HOME/www"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 ls -lah
-#php $SITEPATH/scripts/run-tests.sh --php "/usr/bin/php" --verbose --color --url http://127.0.0.1 --class "TravisDrupalModuleExampleDrupalUnitTestCase"
+php $SITEPATH/scripts/run-tests.sh --php "/usr/bin/php" --verbose --color --url http://127.0.0.1 --class "TravisDrupalModuleExampleDrupalUnitTestCase"
