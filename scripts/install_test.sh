7748fea3a482d910d5bf8202a7d47697a557a51a
Morvaniuk Vitalii <morvanyuk.vitaly@gmail.com>
2016-06-03 09:15:21 +0300

    Setup tests #6

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -6,9 +6,8 @@ DATABASE_PASS="test"
 cd $DOCROOT
 
 drush dl drupal-7 --drupal-project-rename=drupal
-mv drupal/* $DOCROOT
-mv drupal/.htaccess $DOCROOT
-rm -rf drupal
+ls -lah
+pwd
 ln -s $ZENCI_DEPLOY_DIR sites/all/modules/travis_ci_drupal_module_example
 drush --verbose site-install --db-url=mysql://test:@127.0.0.1/test --yes
 drush --yes pm-enable simpletest
